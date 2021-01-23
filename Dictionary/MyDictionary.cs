09e65f5be33a45a9e822a977a648408685428183
Mert CUMALI <mert.cumali@ogr.deu.edu.tr>
2021-01-23 19:45:29 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempKeys;
+        TValue[] tempValues;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+        
+            tempKeys = keys;
+            tempValues = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            values[values.Length - 1] = value;
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                if (key.Equals(tempKeys[i]))
+                {
+                    throw new ArgumentException("An item with the same key has already been added.");
+                }
+            }
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Plates = new MyDictionary<int, string>();
+            Plates.Add(35, "İzmir");
+            Plates.Add(34, "İstanbul");
+            Plates.Add(06, "Ankara");
+            Plates.Add(16, "Bursa");
+            Console.WriteLine(Plates.Count);
+        }
+    }
+}
